e63877b38c1cf2bf8b9907049772888371ac6035
Taibaloprojects <your_email_for_taibaloprojects@domain.com>
2026-02-17 15:20:32 -0500

    chagnes

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,11 +3,12 @@ import { getFirestore, terminate } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDnptPdcUbGMBckDwF0Fw580KzMT6Br79g",
-  authDomain: "thehiregroundpodcast.firebaseapp.com",
-  projectId: "thehiregroundpodcast",
-  storageBucket: "thehiregroundpodcast.firebasestorage.app",
+  authDomain: "gen-lang-client-0527665513.firebaseapp.com", // Updated
+  projectId: "gen-lang-client-0527665513",                // Updated
+  storageBucket: "gen-lang-client-0527665513.firebasestorage.app", // Updated
   messagingSenderId: "221885926112",
-  appId: "1:221885926112:web:868f103682fb0f01641e85"
+  appId: "1:221885926112:web:868f103682fb0f01641e85",
+  measurementId: "G-KEKB8TQX32"
 };
 
 const app = initializeApp(firebaseConfig);
